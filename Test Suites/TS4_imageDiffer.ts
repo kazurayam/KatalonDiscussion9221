e9bf75999f6cebfc0a7721ff3a172b4a18c1f7bf
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-09-06 13:43:01 +0900

    updates

--- Test Suites/TS4_imageDiffer.ts
+++ Test Suites/TS4_imageDiffer.ts
@@ -4,7 +4,7 @@
    <name>TS4_imageDiffer</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-09-06T09:44:13</lastRun>
+   <lastRun>2018-09-06T10:27:10</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
